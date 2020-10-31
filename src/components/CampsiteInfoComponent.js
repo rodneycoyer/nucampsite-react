fcf1171a546c9788d5a673e154d39ebd2ad0e62b
roofwalker13 <roofwalker13@outlook.com>
2020-10-31 12:47:52 -0500

    Week 4 WorkShop -  Button, Modal, LocalFor, Control, Errors, Validation

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+const maxLength = len => val => !(val) || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}) {
     return(
@@ -19,6 +23,8 @@ function RenderCampsite({campsite}) {
     );
 }
 
+
+
 function RenderComments({comments}) {
     if (comments) {
         return (
@@ -31,16 +37,97 @@ function RenderComments({comments}) {
                                 <p>
                                     {comment.text}
                                     <br/>
-                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    -- {comment.author}, 
+                                    {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                                 </p>
                             </div>
                         );
-                    })
-                }
+                    })}
+                <CommentForm />
             </div>
         );
     }
     return <div />;
+}
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+    }
+
+    render () {
+        return (
+            <div>
+                <Button onClick={this.toggleModal} outline >
+                    <i className="fa fa-pencil fa-lg"> Submit Comments </i>
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal} > Submit Comment </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating"> Rating </Label>
+                                <Control.select className="form-control" model=".rating" id="rating" name="rating">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author"> Your Name </Label>
+                                <Control.text className="form-control" model=".author" id="author" name="author" placeholder="Your Name" 
+                                    validators={{
+                                                minLength: minLength(2), 
+                                                maxLength: maxLength(15)
+                                            }}
+
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text"> Comment </Label>
+                                <Control.textarea className="form-control" rows="6" model=".text" id="text" name="text" placeholder="Comments Here" />
+                            </div>
+                            <Button color="primary" type="submit"> Submit </Button> 
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 function CampsiteInfo(props) {
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -80,7 +80,10 @@ class Contact extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 <Col md={10}>
-                                    <Control.text model=".firstName" id="firstName" name="firstName"
+                                    <Control.text
+                                        model=".firstName"
+                                        id="firstName"
+                                        name="firstName"
                                         placeholder="First Name"
                                         className="form-control"
                                         validators={{
@@ -105,7 +108,10 @@ class Contact extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="lastName" md={2}>Last Name</Label>
                                 <Col md={10}>
-                                    <Control.text model=".lastName" id="lastName" name="lastName"
+                                    <Control.text
+                                        model=".lastName"
+                                        id="lastName"
+                                        name="lastName"
                                         placeholder="Last Name"
                                         className="form-control"
                                         validators={{
@@ -130,7 +136,10 @@ class Contact extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="phoneNum" md={2}>Phone</Label>
                                 <Col md={10}>
-                                    <Control.text model=".phoneNum" id="phoneNum" name="phoneNum"
+                                    <Control.text
+                                        model=".phoneNum"
+                                        id="phoneNum"
+                                        name="phoneNum"
                                         placeholder="Phone number"
                                         className="form-control"
                                         validators={{
@@ -157,7 +166,10 @@ class Contact extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="email" md={2}>Email</Label>
                                 <Col md={10}>
-                                    <Control.text model=".email" id="email" name="email"
+                                    <Control.text
+                                        model=".email"
+                                        id="email"
+                                        name="email"
                                         placeholder="Email"
                                         className="form-control"
                                         validators={{
@@ -176,7 +188,8 @@ class Contact extends Component {
                                         }}
                                     />
                                 </Col>
-                            </Row>                           <Row className="form-group">
+                            </Row>
+                            <Row className="form-group">
                                 <Col md={{size: 4, offset: 2}}>
                                     <div className="form-check">
                                         <Label check>
@@ -190,7 +203,9 @@ class Contact extends Component {
                                     </div>
                                 </Col>
                                 <Col md={4}>
-                                    <Control.select model=".contactType" name="contactType"
+                                    <Control.select
+                                        model=".contactType"
+                                        name="contactType"
                                         className="form-control">
                                         <option>By Phone</option>
                                         <option>By Email</option>
@@ -200,7 +215,10 @@ class Contact extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="feedback" md={2}>Your Feedback</Label>
                                 <Col md={10}>
-                                    <Control.textarea model=".feedback" id="feedback" name="feedback"
+                                    <Control.textarea
+                                        model=".feedback"
+                                        id="feedback"
+                                        name="feedback"
                                         rows="12"
                                         className="form-control"
                                     />
